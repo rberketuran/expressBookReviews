f21ce18fd9b6eac644e76e4829c9459268af2b2a
dusty0ne <z3r0.dusty@gmail.com>
2024-06-29 13:21:38 +0300

    Implemented register endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,29 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+function doesExist(username) {
+  let filtered_users = users.filter((user) => username === user.username);
+  if(filtered_users.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if(!username || !password){
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  if(doesExist(username)){
+    return res.status(404).json({message: "User already exists!"});
+  } else {
+    users.push({"username": username, "password": password});
+    console.log(users)
+    return res.status(200).json({message: "User successfully registered. Now you can login"});
+  }
 });
 
 // Get the book list available in the shop
